ee56bde5709b472cf1cf918ea2204827443e7ebf
lt222zi <lt222zi@student.lnu.se>
2026-03-11 11:27:22 +0100

    Liten uppdatering

--- src/ILibraryStore.java
+++ src/ILibraryStore.java
@@ -1,16 +1,19 @@
 import java.util.List;
 
 public interface ILibraryStore {
-    void addBook(Book newBook);
+    void addBook(Book book);
+    void updateBook(Book book);
     Book getBook(String isbn);
     void removeBook(String isbn);
 
-    void addMember(Member newMember);
+    void addMember(Member member);
+    void updateMember(Member member);
     Member getMember(String memberId);
     Member getMemberByPersonalNumber(String personalNumber);
     void removeMember(String memberId);
 
     void addLoan(Loan loan);
+    void updateLoan(Loan loan);
     Loan getActiveLoan(String memberId, String isbn);
     List<Loan> getLoansForMember(String memberId);
     List<Loan> getLoansForBook(String isbn);
